5b656a869a44fe9fb008310ee287889a7afcacfe
Amy Harvey <allenia_lee@hotmail.com>
2020-09-21 11:27:06 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './components/Header.js';
+import TodoContainer from './components/TodoContainer.js';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <TodoContainer />
+    </div>
+  );
+}
+
+export default App;
